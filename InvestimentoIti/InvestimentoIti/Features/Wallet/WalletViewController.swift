a705e3dac6ab1859f40ee0d604134172dd55157f
Ricardo Bailoni <rbailoni@gmail.com>
2020-09-05 15:45:32 -0300

    Update WalletViewController.swift

--- InvestimentoIti/InvestimentoIti/Features/Wallet/WalletViewController.swift
+++ InvestimentoIti/InvestimentoIti/Features/Wallet/WalletViewController.swift
@@ -7,7 +7,63 @@
 //
 
 import UIKit
+import CoreData
 
 class WalletViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     
+    override func viewDidLoad() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        loadInvestiments()
+    }
+    
+    lazy var walletModel: WalletModel = {[weak self] in
+        let walletModel = WalletModel()
+        walletModel.delegate = self
+        return walletModel
+    }()
+    
+    func loadInvestiments() {
+        walletModel.performFetch()
+    }
+    
+    @IBAction func newInvestiment(_ sender: UIButton) {
+        performSegue(withIdentifier: Segue.form, sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Segue.detail {
+            guard let detailViewController = segue.destination as? DetailInvestmentViewController else { return }
+            detailViewController.view.backgroundColor = .blue
+        }
+    }
+}
+
+extension WalletViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        walletModel.totalInvestiments
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? WalletTableViewCell else {
+            return UITableViewCell()
+        }
+        let investiment = walletModel.getInvestimentAt(indexPath)
+        cell.configure(with: investiment)
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+extension WalletViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: Segue.detail, sender: nil)
+    }
+}
+
+extension WalletViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
+    }
 }
